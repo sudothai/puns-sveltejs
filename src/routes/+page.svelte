84f5f3d58c637bd97872d4a9972c01a5c616e0b8
Thai Radin <thai.r.360@gmail.com>
2023-12-03 22:16:32 -0500

    front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center justify-center h-screen">
+    <h1>Prepare to be punned!</h1>
+    <button>Here</button>
+</div>
