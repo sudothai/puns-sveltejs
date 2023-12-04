c471af135d4f32ab54640527d38bd98c1957bb48
Thai Radin <thai.r.360@gmail.com>
2023-12-03 22:40:19 -0500

    blue button and question

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex-col items-center justify-center h-screen">
-    <h1>Prepare to be punned!</h1>
-    <button>Here</button>
+<div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="pb-7 text-2xl">What does a vegetarian zombie say?</h1>
+    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Reveal</button>
 </div>
