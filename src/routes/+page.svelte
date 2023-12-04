c12426b0ec6381e18c273564690c3464f26a4453
Thai Radin <thai.r.360@gmail.com>
2023-12-03 23:17:51 -0500

    installed flowbite for svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
 <div class="flex flex-col items-center justify-center h-screen">
     <h1 class="pb-7 text-2xl">What does a vegetarian zombie say?</h1>
-    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Reveal</button>
+    <button on:click={toggleVisible} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Reveal</button>
 </div>
+
+<script>
+    let visible = true;
+
+    function toggleVisible() {
+        visible = !visible
+        alert('GGGRRRRAAAIIINNNSSS');
+    }
+</script>
