9e71399eb36fb5af76ecf51cb9a010faf0cb6e83
Thai Radin <thai.r.360@gmail.com>
2023-12-04 10:03:29 -0500

    looking good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,7 @@
 <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="pb-7 text-2xl font-bold">What does a vegetarian zombie say?</h1>
-    <Button on:click={toggleVisible} size="lg">Reveal</Button>
+    <h1 class="pb-9 text-5xl font-extrabold">What does a vegetarian zombie say?</h1>
+    <Button on:click={toggleVisible} size="xl" class="text-xl">Reveal</Button>
 </div>
-
-<div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
 
 <script>
     import { Alert } from 'flowbite-svelte';
