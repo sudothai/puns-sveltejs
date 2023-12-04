b289b4333c92fba745612efed505b16b0d2ad4f4
Thai Radin <thai.r.360@gmail.com>
2023-12-03 23:28:24 -0500

    orange button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="pb-7 text-2xl">What does a vegetarian zombie say?</h1>
-    <button on:click={toggleVisible} class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Reveal</button>
+    <h1 class="pb-7 text-2xl font-bold">What does a vegetarian zombie say?</h1>
+    <Button on:click={toggleVisible} size="lg">Reveal</Button>
 </div>
 
+<div class="p-8">
+    <Alert>
+      <span class="font-medium">Info alert!</span>
+      Change a few things up and try submitting again.
+    </Alert>
+  </div>
+
 <script>
+    import { Alert } from 'flowbite-svelte';
+    import { Button } from 'flowbite-svelte';
+
     let visible = true;
 
     function toggleVisible() {
