847750c5d229b7b8250342ab40d9510217987621
Thai Radin <thai.r.360@gmail.com>
2023-12-04 20:45:57 -0500

    got reveal button to work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,23 @@
 <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="pb-9 text-5xl font-extrabold">What does a vegetarian zombie say?</h1>
-    <Button on:click={toggleVisible} size="xl" class="text-xl">Reveal</Button>
+    <Typewriter>
+        <h1 class="pb-9 text-5xl font-extrabold">What does a vegetarian zombie say?</h1>
+    </Typewriter>
+    <Button on:click={toggleVisible} size="xl" class="text-xl" outline>Reveal</Button>
+    {#if visible}
+        <h1 class="pt-9 text-3xl">
+           GGGRRRRAAAIIINNNNSSS 
+        </h1>
+    {/if}
 </div>
 
 <script>
-    import { Alert } from 'flowbite-svelte';
     import { Button } from 'flowbite-svelte';
+    import Typewriter from 'svelte-typewriter';
 
-    let visible = true;
+    let visible = false;
 
     function toggleVisible() {
         visible = !visible
-        alert('GGGRRRRAAAIIINNNSSS');
     }
 </script>
+
