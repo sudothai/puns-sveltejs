1a6169f6fe6b42d2cac9798711b8143991bf201e
Thai Radin <thai.r.360@gmail.com>
2023-12-04 21:06:19 -0500

    added like and dislike button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,23 @@
     </Typewriter>
     <Button on:click={toggleVisible} size="xl" class="text-xl" outline>Reveal</Button>
     {#if visible}
-        <h1 class="pt-9 text-3xl">
+        <h1 class="pt-9 pb-9 text-3xl">
            GGGRRRRAAAIIINNNNSSS 
         </h1>
+        <Button outline={true} class="!p-2 pt-9" size="lg">
+            <ThumbsUpSolid class="w-6 h-6 text-primary-700" />
+        </Button>
+        <Button outline={true} class="!p-2 pt-9" size="lg">
+            <ThumbsDownSolid class="w-6 h-6 text-primary-700" />
+        </Button>
     {/if}
 </div>
 
 <script>
     import { Button } from 'flowbite-svelte';
     import Typewriter from 'svelte-typewriter';
+    import { ThumbsUpSolid, ThumbsDownSolid } from 'flowbite-svelte-icons';
+
 
     let visible = false;
 
