37e261debb6c23f1afce8b397421edbaa65c98fa
Thai Radin <thai.r.360@gmail.com>
2023-12-04 22:42:18 -0500

    added next button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,17 @@
         <h1 class="pt-9 pb-9 text-3xl">
            GGGRRRRAAAIIINNNNSSS 
         </h1>
-        <Button outline={true} class="!p-2 pt-9" size="lg">
-            <ThumbsUpSolid class="w-6 h-6 text-primary-700" />
-        </Button>
-        <Button outline={true} class="!p-2 pt-9" size="lg">
-            <ThumbsDownSolid class="w-6 h-6 text-primary-700" />
-        </Button>
+        <div class="flex flex-row">
+            <Button outline={true} class="!p-2 pt-9" size="lg">
+                <ThumbsUpSolid class="w-6 h-6 text-primary-700" />
+            </Button>
+            <Button outline={true} class="!p-2 pt-9 hover:text-white" size="lg">
+                <ThumbsDownSolid class="w-6 h-6 text-primary-700" />
+            </Button>
+            <Button class="!p-2">
+                <ArrowRightOutline class="w-6 h-6" />
+            </Button>
+        </div>
         <iframe src="https://giphy.com/embed/WOCEirCJycoVzDOiZ2" width="480" height="480" frameBorder="0" class="giphy-embed pt-4" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/plantsvszombies-pvz-pvzbfn-battle-for-neighborville-WOCEirCJycoVzDOiZ2"></a></p>
     {/if}
 </div>
@@ -20,7 +25,7 @@
 <script>
     import { Button } from 'flowbite-svelte';
     import Typewriter from 'svelte-typewriter';
-    import { ThumbsUpSolid, ThumbsDownSolid } from 'flowbite-svelte-icons';
+    import { ThumbsUpSolid, ThumbsDownSolid, ArrowRightOutline } from 'flowbite-svelte-icons';
 
 
     let visible = false;
