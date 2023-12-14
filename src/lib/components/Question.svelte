a4dbd910c14bb7127d634ff128405ade30a5c82a
Thai Radin <Thai.R.360@gmail.com>
2023-12-13 22:52:32 -0500

    working thing

--- src/lib/components/Question.svelte
+++ src/lib/components/Question.svelte
@@ -1,7 +1,12 @@
-<script>
+<script lang="ts">
     import Typewriter from 'svelte-typewriter';
+    // import type { PageData } from "./$types"
+
+    // export let data: PageData
+    // $: ({ puns } = data) 
 </script>
 
 <Typewriter>
-    <h1 class="pt-1 text-3xl font-extrabold dark:text-white">What does a vegetarian zombie say?</h1>
+    <!-- <h1 class="pt-1 text-3xl font-extrabold dark:text-white">{puns.question}</h1> -->
+    <h1 class="pt-1 text-3xl font-extrabold dark:text-white">What do vegetarian zombies say?</h1>
 </Typewriter>
