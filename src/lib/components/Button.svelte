ec56055cbb91baa78030f1af511c2f7445bbc215
Thai Radin <Thai.R.360@gmail.com>
2023-12-13 21:14:58 -0500

    added typewriter to answer

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,6 +2,7 @@
     import { Button } from 'flowbite-svelte';
     import { Card } from 'flowbite-svelte';
     import { ArrowRightOutline } from 'flowbite-svelte-icons';
+    import Typewriter from 'svelte-typewriter';
 
     let visible = false;
 
@@ -13,9 +14,11 @@
 <Button on:click={toggleVisible} size="md" class="text-md mt-7" color="red">Reveal</Button>
 {#if visible}
     <Card class="mt-9">
-        <h2 class="mb-3 text-xl dark:text-white">
-            GGGRRRRAAAIIINNNNSSS 
-        </h2>
+        <Typewriter>
+            <h2 class="mb-3 text-xl dark:text-white">
+                GGGRRRRAAAIIINNNNSSS 
+            </h2>
+        </Typewriter>
         <Button class="flex justify-center items-center" color="red">
             Ready For More?
         </Button>
